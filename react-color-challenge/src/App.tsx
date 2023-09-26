a8df37a92e4d76c9b7dea4ad26110738137c7856
davidbryan <aviles.davidb@gmail.com>
2023-09-25 21:42:46 -0400

    reached out to mock api, added css to button, created initial mapping for creation of buttons

--- react-color-challenge/src/App.tsx
+++ react-color-challenge/src/App.tsx
@@ -1,11 +1,36 @@
+import { useState, useEffect } from "react"
 import styles from "./App.module.css"
+import { getColors } from "./util/mockAPI"
+import { Button } from "./components/button"
 
 function App() {
+
+  const [totalBtnClicks, setTotalBtnClicks] = useState<number>(0)
+  const [colors, setcolors] = useState<string[]>([])
+  useEffect(() => {
+    const getColorList = async () => {
+      try {
+        const colors: string[] = await getColors()
+        setcolors(colors)
+      } catch (error) {
+        alert("Someting went wrong calling Mock API...")
+      }
+    }
+
+    getColorList()
+  }, [])
+
 
   return (
     <>
       <div className={`${styles.container}`}>
         <h1>Color Challenge</h1>
+        <p>Total button Clicks: {totalBtnClicks}</p>
+        <div className={`${styles.innerContainer}`}>
+          {colors.length > 0 && colors.map(color => (
+            <Button color={color} addClick={() => setTotalBtnClicks(prevState => prevState + 1)} />
+          ))}
+        </div>
       </div>
     </>
   )
--- react-color-challenge/src/components/button.tsx
+++ react-color-challenge/src/components/button.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styles from "./button.module.css"
 
-export const Button: React.FC<{ color: string }> = ({ color }) => {
+export const Button: React.FC<{ color: string, addClick: () => void }> = ({ color, addClick }) => {
   return (
-    <div>{color}</div>
+    <div
+      className={`${styles.btn}`}
+      style={{ background: `${color}` }}
+      onClick={() => {
+        addClick;
+        alert(`My color is ${color}`)
+      }}>
+      Click me to see my color's name.</div>
   )
 }
